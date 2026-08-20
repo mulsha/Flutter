685a31be6a7dd6446f0e09d74068a39083074889
mulsha <priyanshimulsha2510@gmail.com>
2026-08-20 11:03:08 +0530

    login_screen_api

--- lib/Practice/bottom_nav.dart
+++ lib/Practice/bottom_nav.dart
@@ -1,49 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:priyanshi/Practice/stack.dart';
 
-import 'gridview.dart';
-import 'listview.dart';
-
-
-class bottom_nav extends StatefulWidget {
-  bottom_nav({super.key});
-
-  @override
-  State<bottom_nav> createState() => _bottom_navState();
-}
-
-class _bottom_navState extends State<bottom_nav> {
-  int currentindex = 0;
-
-  void onTap(int index) {
-    setState(() {
-      currentindex = index;
-    });
-  }
-
-  List screens = [stack(), listview(), gridview()];
+class BottomNavExample extends StatelessWidget {
+  const BottomNavExample({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: screens[currentindex],
-      bottomNavigationBar: BottomNavigationBar(
-        onTap: onTap,
-        currentIndex: currentindex,
-        backgroundColor: Colors.cyan,
-        fixedColor: Colors.white,
-        items: [
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.account_circle),
-            label: "Profile",
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.settings),
-            label: "Settings",
-          ),
-        ],
-      ),
-    );
+    return Scaffold();
   }
 }
